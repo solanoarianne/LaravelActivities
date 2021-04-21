238632141ee5258fa07de11bc13a4a9d3730f1e0
solanoarianne <201811583@gordoncollege.edu.ph>
2021-04-21 21:05:21 +0800

    init

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <a class="btn button btn-info" href="/posts/create">Create New</a>
+            <br><br>
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>                
+            @endif
+            <div class="card">       
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                                <tr>
+                                    <th> ID </th>
+                                    <th> Title </th>
+                                    <th> Description </th>
+                                    <th>  </th>
+                                </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($posts as $post)
+                              <tr>
+                                    <td> {{ $post->id }} </td>
+                                    <td> {{ $post->title }} </td>
+                                    <td> {{ $post->description }} </td>
+                                    <td> <a  href="/posts/{{$post->id}}" class="btn btn-info"> View </a> </td>
+                                    <td> <a  href="/posts/{{$post->id}}/edit" class="btn btn-warning"> Edit </a> </td>
+                                    <td> 
+                                        <form method="POST" action=" {{ route('posts.destroy', $post->id)}}">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                        </form>
+                                    </td>
+                              </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+@endsection
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+           
+            <a class="btn button btn-info" href="/posts/create">Create New</a>
+            
+            <div class="card">       
+                <div class="card-body">
+                    Title : {{ $post->title }} <br>
+                    Description : {{ $post->description }} <br>
+                    Created At : {{ $post->created_at }} <br>
+                    Post Image: 
+                    @if ($post->img)
+                   
+                        <img src="{{ asset('/storage/img/'.$post->img) }} ">
+                    @else
+                        No image available
+                    @endif
+                 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+@endsection
