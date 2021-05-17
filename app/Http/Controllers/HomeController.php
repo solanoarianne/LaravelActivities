918f0ee6ea1586bfd2b96d5cc86ba8641b7b4c60
solanoarianne <201811583@gordoncollege.edu.ph>
2021-05-17 16:33:29 +0800

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth')->except('index');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $posts = Post::where('title','!=','')->orderBy('created_at','desc')->get();
+        $count = Post::where('title','!=','')->count();
+        return view('index', compact('posts', 'count'));
+    }
+}
